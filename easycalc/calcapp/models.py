7afc2a7a00eccfe6844f0808a8bc7266e7d6d319
nneo <aisapple1030@naver.com>
2022-07-29 22:57:00 +0900

    calcapp model 추가

--- easycalc/calcapp/models.py
+++ easycalc/calcapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from account.models import *
 
-# Create your models here.
+class Group(models.Model):
+    user = models.ManyToManyField(related_name='groups')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
+    code = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    is_done = models.BooleanField(default=False)
+
+class Payment(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=30)
+    divide_type = models.CharField(max_length=10, default='1/N')
+    payer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class ToPay(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
--- easycalc/calcapp/views.py
+++ easycalc/calcapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
+import random
 
-# Create your views here.
+def createCode(request):
+    # TODO: owner id + 코드로 만들기
+    code = ''
+    for i in range(3):
+        rand = random.randrange(16, 256)
+        code += hex(rand)[2:]
+
+def createGroup(request):
+    if request.method == 'POST':
+        group = Group()
+        group.name = request.POST['name']
+        # TODO: 중복 코드인지 검사 
+        group.code = createCode()
+        
