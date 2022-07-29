6c4a3c0703df44703957df9e78f04e1344cfa680
nneo <aisapple1030@naver.com>
2022-07-30 03:10:17 +0900

    merge 후 urls.py 수정

--- easycalc/calcapp/urls.py
+++ easycalc/calcapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from calcapp import views
 
+app_name = 'calcapp'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:pk>/', views.calc_result),
     path('group/', views.create, name='creategroup'),
     path('payment/<int:group_id>', views.create, name='createpayment'),
 ]
-app_name = 'calcapp'
-    path('<int:pk>/', views.calc_result),
